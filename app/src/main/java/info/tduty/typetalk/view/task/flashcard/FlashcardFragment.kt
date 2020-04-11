50344b181be18c5c9c9b2eeeb898e677afb6f31e
Doomsday46 <Gallifrey46@gmail.com>
2020-04-12 00:05:19 +0300

    setup navigation bar, delete fix card shadow

--- app/src/main/java/info/tduty/typetalk/view/task/flashcard/FlashcardFragment.kt
+++ app/src/main/java/info/tduty/typetalk/view/task/flashcard/FlashcardFragment.kt
@@ -3,7 +3,13 @@ package info.tduty.typetalk.view.task.flashcard
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
@@ -11,7 +17,10 @@ import androidx.viewpager2.widget.ViewPager2
 import info.tduty.typetalk.App
 import info.tduty.typetalk.R
 import info.tduty.typetalk.data.model.FlashcardVO
+import info.tduty.typetalk.view.MainActivity
+import info.tduty.typetalk.view.ViewNavigation
 import info.tduty.typetalk.view.task.flashcard.di.FlashcardModule
+import kotlinx.android.synthetic.main.fragment_dictionary.view.*
 import kotlinx.android.synthetic.main.fragment_task_flashcard.*
 import javax.inject.Inject
 
@@ -47,10 +56,34 @@ class FlashcardFragment: Fragment(R.layout.fragment_task_flashcard), FlashcardVi
         val lessonId = arguments?.getString(ARGUMENT_LESSON_ID)
             ?: throw IllegalArgumentException("lesson id is null")
 
+        setupToolbar(view.toolbar as Toolbar, R.string.task_screen_default_title, true)
+        setHasOptionsMenu(true)
+
         setupViewPager()
         setupListener()
 
         presenter.onCreate(lessonId)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_dictionary -> (activity as? ViewNavigation)?.openDictionary()
+            R.id.action_chat -> (activity as? ViewNavigation)?.openTeacherChat()
+        }
+        return true
+    }
+
+    private fun setupToolbar(toolbar: Toolbar, @StringRes title: Int, withBackButton: Boolean) {
+        toolbar.setTitle(title)
+        (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
+        (activity as? AppCompatActivity)?.supportActionBar?.setHomeButtonEnabled(withBackButton)
+        (activity as? AppCompatActivity)?.supportActionBar?.setDisplayHomeAsUpEnabled(withBackButton)
     }
 
     private fun setupFragmentComponent() {
